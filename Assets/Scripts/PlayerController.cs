c8c9462f2b7e08e933944d3c5991fca7f48231e1
liyu050718 <2820026889@qq.com>
2025-12-06 01:07:13 +0800

    qqq

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private int currentDir = 0;
+    public int dir = 0;        
     public bool ismoving = false;
     public bool haveKey;
     public float speed;
@@ -16,31 +18,63 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-        anim.SetFloat("Velocityx", rb.velocity.x);
-        anim.SetFloat("Velocityy", rb.velocity.y);
-        if (Input.GetKey(KeyCode.W))
         {
-            rb.velocity = Vector2.up * speed;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            rb .velocity = Vector2.down * speed;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            rb.velocity = Vector2.right * speed;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            rb.velocity = Vector2.left * speed;
-        }
-        else
-        {
-            ismoving = false;
-        }
-        ismoving = true;
+            anim.SetFloat("Velocityx", rb.velocity.x);
+            anim.SetFloat("Velocityy", rb.velocity.y);
 
-        anim.SetBool("IsMoving", ismoving);
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.velocity = Vector2.up * speed;
+                dir = 0;
+
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity = Vector2.down * speed;
+                dir = 1;
+
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                rb.velocity = Vector2.right * speed;
+                dir = 2;
+
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                rb.velocity = Vector2.left * speed;
+                dir = 3;
+                //transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+            ismoving = true;
+            if (dir != currentDir)
+            {
+                if (dir == 0)
+                {
+                    anim.Play("IdleBa");
+                }
+                else if (dir == 1)
+                {
+                    anim.Play("IdleZH");
+                }
+                else if (dir == 2)
+                {
+                    anim.Play("IdleYOUCE");
+                }
+                else if (dir == 3)
+                {
+                    anim.Play("IdleZUOCe");
+                }
+                if (dir == 3 || currentDir == 3)
+                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                currentDir = dir;
+            }
+            anim.SetBool("IsMoving", ismoving);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -52,6 +86,10 @@ public class PlayerController : MonoBehaviour
         if(canGetKey&&Input.GetKeyDown(KeyCode.E))
         {
             haveKey = true;
+        }
+        if(collision.tag == "Ci")
+        {
+            Debug.Log("À¿¿≤");
         }
     }
 }
